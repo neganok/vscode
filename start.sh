8e4bcb4bc58d995238693a6dd7e4612a7b60cfb9
Văn Thành <hackersvn1@gmail.com>
2025-03-16 14:00:26 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -23,14 +23,9 @@ while [ $total_seconds -gt 0 ]; do
     hours=$((total_seconds / 3600))
     minutes=$(( (total_seconds % 3600) / 60 ))
     seconds=$((total_seconds % 60))
-    printf "Thời gian còn lại trước khi chạy update.py: %02d giờ %02d phút %02d giây\n" $hours $minutes $seconds
+    printf "Thời gian còn lại: %02d giờ %02d phút %02d giây\n" $hours $minutes $seconds
     sleep 1
     total_seconds=$((total_seconds - 1))
 done
 
-# Sau 29 phút, chạy update.py và đợi nó hoàn thành
-python3 update.py
-wait $!  # Đợi cho đến khi update.py hoàn thành
 
-# Sau khi update.py hoàn thành, dừng tất cả các tiến trình liên quan đến start.sh
-pkill -f -9 start.sh
